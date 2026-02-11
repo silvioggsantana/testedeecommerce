1feb93257bf67f479816d8b6433dee8898e690fe
Silvio Gabriel <silvioggsantana@gmail.com>
2026-02-11 19:41:02 -0300

    resolvendo problemas de banco do deploy

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from './prisma.config';  // Ajuste path
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends prisma.$extends({}) implements OnModuleInit, OnModuleDestroy {  // Extensível para custom queries
   async onModuleInit() {
     await this.$connect();
   }
